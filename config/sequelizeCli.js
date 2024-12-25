0c8c1214ab117ff0dbdfa3997d3b70ddf9986a18
David Pablo Cardoso Breda Queiroz <Davidqueiroz@192.168.100.3>
2024-12-25 12:24:40 -0400

    correções

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,16 +1,12 @@
 // config/sequelizeCli.js
-import { sequelize } from '../database'
-import { adminJsResources } from './resources'
 
 module.exports = {
-    development: {
-      dialect: 'postgres',
-      host: 'localhost',
-      port: '5432',
-      resources: adminJsResources,
-      rootPath: '/admin',
-      databases: [sequelize],
-      username: 'onebitflix',
-      password: 'onebitflix'
-    }
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'onebitflix_development',
+    username: 'onebitflix',
+    password: 'onebitflix'
   }
+}
--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -20,7 +20,7 @@ export const adminJs = new AdminJs({
 	      primary80: '#ff1a57',
 	      primary60: '#ff3369',
 	      primary40: '#ff4d7c',
-		    primary20: '#ff668f',
+			primary20: '#ff668f',
 	      grey100: '#151515',
 	      grey80: '#333333',
 	      grey60: '#4d4d4d',
