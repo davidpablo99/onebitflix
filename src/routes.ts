0a2b84fb455f996bf29c75cbc2ee012a6eeefb3a
David Pablo Cardoso Breda Queiroz <Davidqueiroz@192.168.100.3>
2025-01-03 07:31:44 -0400

    obtendo cursos de uma categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
-import express from "express"
-import { categoriesController } from "./controllers/categoriesController"
+// src/routes.ts
+
+import express from 'express'
+import { categoriesController } from './controllers/categoriesController'
 
 const router = express.Router()
-router.get('/categories', categoriesController.index)
 
-export {router}
+router.get('/categories', categoriesController.index)
+router.get('/categories/:id', categoriesController.show)
+
+export { router }
